0d249ae0b50c0024db5c5eb0328a80b6771a1f0c
fptra28 <faturrahman86.fr@gmail.com>
2024-06-17 19:32:03 +0700

    Add Alaert

--- src/porto/pages/dasboard.jsx
+++ src/porto/pages/dasboard.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Jumbotron from "../components/jumbotron";
 import Porto from "../components/portfolioDashboard";
 import Tools from "../components/tools-skills";
 import LayOut from "../components/layout/layout";
 
 function Home() {
+  useEffect(() => {
+    const isMobileOrTablet = window.matchMedia("(max-width: 1023px)").matches;
+    if (isMobileOrTablet) {
+      alert(
+        "Silakan buka halaman ini di desktop view untuk pengalaman terbaik."
+      );
+    }
+  }, []);
+
   return (
     <LayOut>
       <Jumbotron />
