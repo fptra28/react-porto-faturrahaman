5628d52c868850a7e1f53aa0a8ef48b36601cadd
fptra28 <faturrahman86.fr@gmail.com>
2024-06-17 19:23:43 +0700

    Lanjutan

--- src/porto/components/footer.jsx
+++ src/porto/components/footer.jsx
@@ -5,29 +5,29 @@ const Footer = () => {
   return (
     <div className="w-full px-0 bg-white shadow flex flex-col justify-center items-center font-primary">
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-full py-7 bg-white flex justify-center items-center flex-wrap px-20">
+        <div className="w-full py-7 bg-white flex justify-center items-center flex-wrap px-20 text-sm m:text-base">
           <Link
             to="/"
-            className="text-black text-base font-bold hover:text-slate-800 mx-4 my-2"
+            className="text-black font-bold hover:text-slate-800 mx-4 my-2"
           >
             HOME
           </Link>
           <Link
             to="/portofolio"
-            className="text-black text-base font-bold hover:text-slate-800 mx-4 my-2"
+            className="text-black font-bold hover:text-slate-800 mx-4 my-2"
           >
             PORTFOLIO
           </Link>
           <Link
             to="/about"
-            className="text-black text-base font-bold hover:text-slate-800 mx-4 my-2"
+            className="text-black font-bold hover:text-slate-800 mx-4 my-2"
           >
             ABOUT
           </Link>
         </div>
 
         <div className="w-full h-px border-t border-black"></div>
-        <div className="w-full py-7 flex justify-center items-center text-black text-xs font-bold text-center sm:text-lg">
+        <div className="w-full py-7 flex justify-center items-center text-black text-sm font-bold text-center sm:text-lg">
           COPYRIGHT © 2024. Made with ❤️ by Senfuri0n
         </div>
       </div>
