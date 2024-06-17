3243056d43ddc72c6c3747baa2f586e3f2104c21
fptra28 <faturrahman86.fr@gmail.com>
2024-06-17 19:24:26 +0700

    Lanjutan

--- src/porto/components/footer.jsx
+++ src/porto/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="w-full px-0 bg-white shadow flex flex-col justify-center items-center font-primary">
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-full py-7 bg-white flex justify-center items-center flex-wrap px-20 text-sm m:text-base">
+        <div className="w-full py-7 bg-white flex justify-center items-center flex-wrap px-20 text-sm md:text-base">
           <Link
             to="/"
             className="text-black font-bold hover:text-slate-800 mx-4 my-2"
